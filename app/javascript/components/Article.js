27d158d0557e317d1ae52e87991f2ac71d244bc9
Presley McQuade Nogueria Costa <mcquadenc@gmail.com>
2019-10-16 20:06:47 -0400

    Aplicando material ui

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,32 +1,61 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
 class Article extends React.Component {
-  render () {
+  render() {
     return (
       <React.Fragment>
+        {/* Material App Bar */}
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" style={{flex: 1}}>
+              Listing articles
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
 
-	<h1>Listing articles</h1>
-
-	<table>
-	 <thead>
-  		  <tr>
-		    <th>Title</th>
-		    <th>Text</th>
-		    <th></th>
-		  </tr>
-	 </thead>
-	 <tbody>
+        <table>
+         <thead>
+              <tr>
+              <th>Title</th>
+              <th>Text</th>
+              <th></th>
+            </tr>
+         </thead>
+         <tbody>
 
 
-	          {this.props.articles.map(article => (
-		  <tr key={article.id}>
-        	    <td>{`${article.title}`}</td>
-        	    <td>{`${article.text}`}</td>
-		  </tr>
-	          ))}
+                  {this.props.articles.map(article => (
+            <tr key={article.id}>
+                    <td>{`${article.title}`}</td>
+                    <td>{`${article.text}`}</td>
+            </tr>
+                  ))}
 
-	 </tbody>
-	</table>
+         </tbody>
+        </table>
+        
+        {/* Material TextField */}
+        <TextField
+          id="name"
+          label="Type here..."
+        />
+
+        {/* Material Button */}
+        <Button variant="raised" color="primary" style={{ marginLeft: 10 }}>
+          Material button
+        </Button>
       </React.Fragment>
     );
   }
@@ -35,4 +64,6 @@ class Article extends React.Component {
 Article.propTypes = {
   articles: PropTypes.array
 };
-export default Article
+
+
+export default Article;
