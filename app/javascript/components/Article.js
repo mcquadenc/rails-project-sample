3cc060d4599839f860bbadba31c9ee5bc8aba702
Presley McQuade Nogueria Costa <mcquadenc@gmail.com>
2019-10-16 20:39:46 -0400

    Usando material table

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -8,8 +8,33 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    width: '100%',
+    overflowX: 'auto',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
+
+
 class Article extends React.Component {
   render() {
+
     return (
       <React.Fragment>
         {/* Material App Bar */}
@@ -25,37 +50,26 @@ class Article extends React.Component {
           </Toolbar>
         </AppBar>
 
-        <table>
-         <thead>
-              <tr>
-              <th>Title</th>
-              <th>Text</th>
-              <th></th>
-            </tr>
-         </thead>
-         <tbody>
-
-
-                  {this.props.articles.map(article => (
-            <tr key={article.id}>
-                    <td>{`${article.title}`}</td>
-                    <td>{`${article.text}`}</td>
-            </tr>
-                  ))}
-
-         </tbody>
-        </table>
-        
-        {/* Material TextField */}
-        <TextField
-          id="name"
-          label="Type here..."
-        />
-
-        {/* Material Button */}
-        <Button variant="raised" color="primary" style={{ marginLeft: 10 }}>
-          Material button
-        </Button>
+	    <div >
+	      <Paper >
+	        <Table  size="small" aria-label="a dense table">
+	          <TableHead>
+	            <TableRow>
+	              <TableCell align="right">Título</TableCell>
+	              <TableCell align="right">Texto</TableCell>
+	            </TableRow>
+	          </TableHead>
+	          <TableBody>
+	            {this.props.articles.map(article => (
+	              <TableRow key={article.id}>
+	                <TableCell align="right">{`${article.title}`}</TableCell>
+	                <TableCell align="right">{`${article.text}`}</TableCell>
+	              </TableRow>
+	            ))}
+	          </TableBody>
+	        </Table>
+	      </Paper>
+	    </div>
       </React.Fragment>
     );
   }
